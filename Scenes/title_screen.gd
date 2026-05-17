96842b7da21f7d314c83e7ad6caa21c39faae8f0
bcmd <54084132+boner-cmd@users.noreply.github.com>
2026-05-17 16:22:58 -0400

    removing unused _process function call

--- Scenes/title_screen.gd
+++ Scenes/title_screen.gd
@@ -6,11 +6,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
 func _on_new_game_button_pressed() -> void:
 	get_tree().change_scene_to_file('res://Scenes/MainScene.tscn')
 
