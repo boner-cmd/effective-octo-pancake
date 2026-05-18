732b85d897b88edb2751bbe1d175df1875be2b00
bcmd <54084132+boner-cmd@users.noreply.github.com>
2026-05-18 15:08:39 -0400

    added planet signal

--- Scenes/confirmation_dialog.gd
+++ Scenes/confirmation_dialog.gd
@@ -1,11 +1,26 @@
 extends TextureRect
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
+# func _input(event):
+#	if event.is_action_just_pressed("ui_accept"):
+#		self.visible = false
+		# go to the other planet
+		
+#	if event.is_action_just_pressed("ui_cancel"):
+#		self.visible = false
+		# unset the current planet
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_yeah_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_nah_button_pressed() -> void:
+	pass # Replace with function body.
--- Scenes/map_ui.gd
+++ Scenes/map_ui.gd
@@ -1,6 +1,14 @@
 extends TextureRect
 
+var	planet_id : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_planet_pressed(planet_id: int) -> void:
+	print(planet_id)
