5849783ffd6d5a0e1980af9367e202826fbe106c
bcmd <54084132+boner-cmd@users.noreply.github.com>
2026-05-18 16:00:11 -0400

    minimal script for map is set up, need to continue with layout

--- Scenes/map_ui.gd
+++ Scenes/map_ui.gd
@@ -2,13 +2,15 @@ extends TextureRect
 
 var	planet_id : int
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-	
+func go_to_planet():
+	visible = false
+	print("DEBUG: FLYING TO PLANET ID " + str(planet_id))
+	# go to other planet
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _on_planet_pressed(planet_id: int) -> void:
-	print(planet_id)
+func _on_planet_pressed(selected_planet_id: int) -> void:
+	planet_id = selected_planet_id
+	get_node(^"ConfirmationDialog").visible = true
