ec2416931a81fc0f983c90a3e769d74effb15ebb
ROCINANTE\mbrissie <mbrissie@gmail.com>
2026-05-18 23:04:10 -0400

    player controller working! weeeeeeeeee! anim tree functional

--- Character Scenes/PlayerController.gd
+++ Character Scenes/PlayerController.gd
@@ -14,7 +14,69 @@ var _camera_input_direction := Vector2.ZERO
 
 @export var planet : Node3D
 
-@onready var clown: Node3D = $MeshInstance3D
+@onready var clown: Node3D = $ClownRigFBX
+
+
+#anim handling
+var anim_tree : AnimationTree
+
+var blend_speed := 50.0
+
+enum {IDLE, WALK, JUMP, GET, GIVE, TALK, VICTORY}
+var current_anim = IDLE
+var walk_val : float = 0.0
+
+var talk_val : float = 0.0
+var get_val : float = 0.0
+var give_val : float = 0.0
+
+
+func handle_animations(delta):
+	match current_anim:
+		IDLE:
+			walk_val = lerpf(walk_val, 0.0, blend_speed * delta)
+			talk_val = lerpf(talk_val, 0.0, blend_speed * delta)
+			get_val = lerpf(get_val, 0.0, blend_speed * delta)
+			give_val = lerpf(give_val, 0.0, blend_speed * delta)
+		WALK:
+			walk_val = lerpf(walk_val, 1.0, blend_speed * delta)
+			talk_val = lerpf(talk_val, 0.0, blend_speed * delta)
+			get_val = lerpf(get_val, 0.0, blend_speed * delta)
+			give_val = lerpf(give_val, 0.0, blend_speed * delta)
+		JUMP:
+			walk_val = lerpf(walk_val, 0.0, blend_speed * delta)
+			talk_val = lerpf(talk_val, 0.0, blend_speed * delta)
+			get_val = lerpf(get_val, 0.0, blend_speed * delta)
+			give_val = lerpf(give_val, 0.0, blend_speed * delta)
+		TALK:
+			walk_val = lerpf(walk_val, 0.0, blend_speed * delta)
+			talk_val = lerpf(talk_val, 1.0, blend_speed * delta)
+			get_val = lerpf(get_val, 0.0, blend_speed * delta)
+			give_val = lerpf(give_val, 0.0, blend_speed * delta)
+		GET:
+			walk_val = lerpf(walk_val, 0.0, blend_speed * delta)
+			talk_val = lerpf(talk_val, 0.0, blend_speed * delta)
+			get_val = lerpf(get_val, 1.0, blend_speed * delta)
+			give_val = lerpf(give_val, 0.0, blend_speed * delta)
+		GIVE:
+			walk_val = lerpf(walk_val, 0.0, blend_speed * delta)
+			talk_val = lerpf(talk_val, 0.0, blend_speed * delta)
+			get_val = lerpf(get_val, 0.0, blend_speed * delta)
+			give_val = lerpf(give_val, 1.0, blend_speed * delta)
+		VICTORY:
+			walk_val = lerpf(walk_val, 0.0, blend_speed * delta)
+			talk_val = lerpf(talk_val, 0.0, blend_speed * delta)
+			get_val = lerpf(get_val, 0.0, blend_speed * delta)
+			give_val = lerpf(give_val, 0.0, blend_speed * delta)
+
+func update_tree():
+	if is_on_floor():
+		anim_tree["parameters/Walk/blend_amount"] = walk_val
+		anim_tree["parameters/Talk/blend_amount"] = talk_val
+		anim_tree["parameters/Get/blend_amount"] = get_val
+		anim_tree["parameters/Give/blend_amount"] = give_val
+	
+
 
 var move_direction : Vector3
 
@@ -39,7 +101,12 @@ func _input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
+		
+	if is_on_floor() and event.is_action_pressed("jump"):
+		#velocity = up_direction * 5
+		current_anim = JUMP
+		anim_tree.set("parameters/Jump/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	
 
 func _unhandled_input(event: InputEvent) -> void:
 	var is_camera_motion := (
@@ -49,8 +116,14 @@ func _unhandled_input(event: InputEvent) -> void:
 	if is_camera_motion:
 		_camera_input_direction = event.screen_relative * mouse_sensitivity
 
+func _ready() -> void:
+	anim_tree = clown.get_node("AnimationTree")
 
 func _physics_process(delta: float) -> void:
+	print(velocity)
+	handle_animations(delta)
+	update_tree()
+	
 	_camera_pivot.rotation.x -= _camera_input_direction.y * delta
 	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, tilt_lower_limit, tilt_upper_limit)
 	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
@@ -66,9 +139,13 @@ func _physics_process(delta: float) -> void:
 
 	#rotate char mesh
 	if raw_input != Vector2(0,0):
-		clown.rotation.y = lerp_angle(clown.rotation.y, _camera_pivot.rotation.y - deg_to_rad(rad_to_deg(raw_input.angle()) + 90), .2)
-
+		clown.rotation.y = lerp_angle(clown.rotation.y, _camera_pivot.rotation.y - deg_to_rad(rad_to_deg(raw_input.angle()) + 90), 1.0)
+		current_anim = WALK
+	else:
+		current_anim = IDLE
+		
 	grav_calc()
+		
 	
 	#if not is_on_floor():
 		#velocity = grav_vector
@@ -80,8 +157,7 @@ func _physics_process(delta: float) -> void:
 	align_with_floor($RayCast3D.get_collision_normal())
 	global_transform = global_transform.interpolate_with(xform, .3)
 	
-	if is_on_floor() and Input.is_action_pressed("jump"):
-		velocity = up_direction * 5
+
 	
 	
 	move_and_slide()
