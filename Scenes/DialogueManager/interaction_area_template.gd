48d7a3e4f5bef6efc15708d7739681b53ebb063d
ROCINANTE\mbrissie <mbrissie@gmail.com>
2026-05-22 19:09:57 -0400

    cleaning and commenting

--- Scenes/DialogueManager/interaction_area_template.gd
+++ Scenes/DialogueManager/interaction_area_template.gd
@@ -4,24 +4,26 @@ extends Node3D
 @onready var CanvasLayer_in: CanvasLayer = %CanvasLayer
 @onready var speech_sound = preload("res://sound fx exports/typewriter2026-05-20_13_26_04.wav")
 
-
+#change to group check also
 var current_NPC : MeshInstance3D
 
+#irrelevant based on quest system but needs implementation
 var NPC_Normal_Template_Check : bool
 
-#COMPLETE is to escape the current loop
+#COMPLETE is to escape the current loop, might be redundant
+#these lines to pass through should rely on global dialogue script
 var lines: Array[String] = []
 var lines_2: Array[String] = []
 var lines_3: Array[String] = []
 
-#these are populated when 
+#these are obsolete based on global dialogue script
 var initial_lines: Array[String] = []
 var give_lines: Array[String] = []
 var receive_lines: Array[String] = []
 var post_lines: Array[String] = []
 var easter_lines: Array[String] = []
 
-#placeholder status
+#placeholder status for conversation state checking MIGHT be obsolete based on quest status
 var temp_status : DialogueManager.CONV_STATE
 var current_status: DialogueManager.CONV_STATE = DialogueManager.CONV_STATE.PLAYER_LISTEN:
 	set(set_current_status):
@@ -56,6 +58,7 @@ func conversation_dialogue():
 	print(lines)
 
 
+#this should only run if child is an NPC
 func interact() -> void:
 	conversation_dialogue()
 	current_NPC.current_state = current_status
@@ -66,6 +69,7 @@ func interact() -> void:
 		lines_2 = []
 		lines_3 = []
 
+#this needs to change to check group and pull lines from global dialogue
 func _ready() -> void:
 	current_NPC = get_child(2)
 	NPC_Normal_Template_Check = current_NPC.NPC_Normal_Template_Check
@@ -77,6 +81,8 @@ func _ready() -> void:
 	post_lines = current_NPC.post_lines
 	easter_lines = current_NPC.easter_lines
 
+
+#this was important for a reason I don't remember probably debugging
 func _on_canvas_layer_child_exiting_tree(_node: Node) -> void:
 	if !DialogueManager.is_dialogue_active:
 		current_status = temp_status
