de95e528e25c493e1f57c8780dc3787b68148073
ROCINANTE\mbrissie <mbrissie@gmail.com>
2026-05-23 23:04:36 -0400

    added a lock state to the door handler for blocked paths, import files

--- Character Scenes/DoorAnimHandler.gd
+++ Character Scenes/DoorAnimHandler.gd
@@ -12,43 +12,48 @@ const sfx_exit = preload("uid://dklltp1vyr8pp")
 @onready var anim_tree: AnimationTree = $AnimationTree
 @onready var player_exit_position: Node3D = $"../PlayerAnimationPosition"
 
+var door_locked : bool = false
+
 enum AnimStates {IDLE, STASIS, SPAWN, DESPAWN, EXIT}
 
 var current_anim := AnimStates.STASIS
 
 func _set_door_anim(anim : AnimStates):
 	current_anim = anim
-	match current_anim:
-		AnimStates.STASIS:
-			anim_tree.set("parameters/Door_Active/blend_amount", 0.0)
-			
-		AnimStates.SPAWN:
-			spawn_sound_player()
-			anim_tree.set("parameters/Reset_DoorSpawn/seek_request", 0.0)
-			anim_tree.set("parameters/DoorSpawnTimescale/scale", 2.0)
-			anim_tree.set("parameters/Door_Active/blend_amount", 1.0)
-			await anim_tree.animation_finished
-			_set_door_anim(AnimStates.IDLE)
-			
-		AnimStates.IDLE:
-			anim_tree.set("parameters/DoorIdle_Reset/seek_request", 0.0)
-			anim_tree.set("parameters/Door_Idle/blend_amount", 0.0)
+	if !door_locked:
+		match current_anim:
+			AnimStates.STASIS:
+				anim_tree.set("parameters/Door_Active/blend_amount", 0.0)
+				
+			AnimStates.SPAWN:
+				spawn_sound_player()
+				anim_tree.set("parameters/Reset_DoorSpawn/seek_request", 0.0)
+				anim_tree.set("parameters/DoorSpawnTimescale/scale", 2.0)
+				anim_tree.set("parameters/Door_Active/blend_amount", 1.0)
+				await anim_tree.animation_finished
+				_set_door_anim(AnimStates.IDLE)
+				
+			AnimStates.IDLE:
+				anim_tree.set("parameters/DoorIdle_Reset/seek_request", 0.0)
+				anim_tree.set("parameters/Door_Idle/blend_amount", 0.0)
 
-		AnimStates.DESPAWN:
-			despawn_sound_player()
-			anim_tree.set("parameters/Door_Idle/blend_amount", 1.0)
-			anim_tree.set("parameters/Reset_DoorSpawn/seek_request", 1.0)
-			anim_tree.set("parameters/DoorSpawnTimescale/scale", -2.0)
-			
-		AnimStates.EXIT:
-			exit_sound_player()
-			exit_anim_started.emit()
-			anim_tree.set("parameters/Reset_DoorExit/seek_request", 0.0)
-			anim_tree.set("parameters/DoorExit/blend_amount", 1.0)
-			await anim_tree.animation_finished
-			exit_anim_finished.emit()
-			_set_door_anim(AnimStates.STASIS)
-			
+			AnimStates.DESPAWN:
+				despawn_sound_player()
+				anim_tree.set("parameters/Door_Idle/blend_amount", 1.0)
+				anim_tree.set("parameters/Reset_DoorSpawn/seek_request", 1.0)
+				anim_tree.set("parameters/DoorSpawnTimescale/scale", -2.0)
+				
+			AnimStates.EXIT:
+				exit_sound_player()
+				exit_anim_started.emit()
+				anim_tree.set("parameters/Reset_DoorExit/seek_request", 0.0)
+				anim_tree.set("parameters/DoorExit/blend_amount", 1.0)
+				await anim_tree.animation_finished
+				exit_anim_finished.emit()
+				_set_door_anim(AnimStates.STASIS)
+	else:
+		#maybe need some weird animation state for polish
+		pass
 
 func _ready() -> void:
 	_set_door_anim(AnimStates.STASIS)
@@ -64,7 +69,6 @@ func despawn():
 	_set_door_anim(AnimStates.DESPAWN)
 
 func _on_door_spawn_radius_area_entered(_area: Area3D) -> void:
-	print("entered")
 	_set_door_anim(AnimStates.SPAWN)
 
 func _on_door_spawn_radius_area_exited(_area: Area3D) -> void:
