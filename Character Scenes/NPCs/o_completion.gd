432b6856d5829dbff5794157ec583e526605b660
ROCINANTE\mbrissie <mbrissie@gmail.com>
2026-05-26 23:41:14 -0400

    cleanup, fix some completion states, grease puddle still incorrect

--- Character Scenes/NPCs/o_completion.gd
+++ Character Scenes/NPCs/o_completion.gd
@@ -2,11 +2,13 @@ extends Node3D
 @onready var q: Node3D = $"../q"
 @onready var cubed_planet: Node3D = $"../../../Cubed_Planet"
 @onready var cubed_skybox: Node3D = $"../../../CubedSkybox"
-const Q_PLANET = preload("uid://dk8lja3wpiagu")
-const Q_SKYBOX = preload("uid://qgwupr3ubecg")
+@onready var cubed_skybox_2: Node3D = $"../../../CubedSkybox2"
+@onready var cubed_planet_2: Node3D = $"../../../Cubed_Planet2"
 
 func on_completion():
 	visible = false
 	q.visible = true
-	cubed_planet.cubed_planet.material_override = Q_PLANET
-	cubed_skybox.cubed_skybox.material_override = Q_SKYBOX
+	cubed_planet.visible = false
+	cubed_skybox.visible = false
+	cubed_planet_2.visible = true
+	cubed_skybox_2.visible = true
