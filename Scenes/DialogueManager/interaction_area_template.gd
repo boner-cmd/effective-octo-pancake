7d99fd93286b575e8cda22b9b5df47e732af4cd3
ROCINANTE\mbrissie <mbrissie@gmail.com>
2026-05-27 18:54:57 -0400

    audio manager half implementation, need to fix dialogue manager sfx passthrough

--- Scenes/DialogueManager/interaction_area_template.gd
+++ Scenes/DialogueManager/interaction_area_template.gd
@@ -2,14 +2,14 @@ extends Node3D
 
 @onready var interact_ui : MarginContainer = %interact
 @onready var CanvasLayer_in : CanvasLayer = %CanvasLayer
-@onready var main_planet_id : int = get_tree().root.get_child(2).public_planet_id
-@onready var speech_sound = preload("res://sound fx exports/typewriter2026-05-20_13_26_04.wav")
+@onready var main_planet_id : int = get_tree().root.get_child(3).public_planet_id
+@onready var speech_sound = AudioManager.speech_sound
 
 #this should only run if child is an NPC
 func interact() -> void:
 	var current_status: DialogueManager.CONV_STATE = DialogueManager.dialogue_state
 	if current_status == DialogueManager.CONV_STATE.COMPLETE:
-		main_planet_id = get_tree().root.get_child(2).public_planet_id
+		main_planet_id = get_tree().root.get_child(3).public_planet_id
 		DialogueManager.start_dialogue(CanvasLayer_in, main_planet_id, speech_sound)
 		
 		for NPC in get_children():
