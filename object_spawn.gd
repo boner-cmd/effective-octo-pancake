b7279245b8ba8a61a74d777496ed1d09602b67dd
ROCINANTE\mbrissie <mbrissie@gmail.com>
2026-05-28 17:06:52 -0400

    adding in extra slot for horse to spawn locations on planets

--- object_spawn.gd
+++ object_spawn.gd
@@ -20,30 +20,35 @@ Vector3(349.0, 0.0, 268.0),
 Vector3(341.0, 0.0, 180.0),
 Vector3(333.0, 0.0, 21.0),
 Vector3(266.0, 0.0, 356.0),
+Vector3(22.0, 0.0, 83.0),
 ]
 const obj_pos_2_const : Array =[
 Vector3(298.0, 0.0, 96),
 Vector3(317.0, 0.0, 294.0),
 Vector3(332.0, 0.0, 198.0),
 Vector3(212.0, 0.0, 189.0),
+Vector3(241.0, 0.0, 0.0),
 ]
 const obj_pos_3_const : Array =[
 Vector3(293.0, 0.0, 235.0),
 Vector3(247.0, 0.0, 164.0),
 Vector3(15.0, 0.0, 98),
 Vector3(206.0, 0.0, 4.0),
+Vector3(300.0, 0.0, 0.0),
 ]
 const obj_pos_4_const : Array =[
 Vector3(348.0, 0.0, 200.0),
 Vector3(102.0, 0.0, 232.0),
 Vector3(171.0, 0.0, 97),
 Vector3(152.0, 0.0, 155.0),
+Vector3(45.0, 0.0, 0.0),
 ]
 const obj_pos_5_const : Array =[
 Vector3(119.0, 0.0, 206.0),
 Vector3(334.0, 0.0, 205.0),
 Vector3(24.0, 0.0, 352.0),
 Vector3(17.0, 0.0, 126.0),
+Vector3(78.0, 0.0, 37.0),
 ]
 
 var obj_pos_1 : Array
@@ -85,9 +90,8 @@ func placement():
 		Obj_3.rotation_degrees = order.pop_back()
 	if Obj_4:
 		Obj_4.rotation_degrees = order.pop_back()
-	#Placeholder junk until can re-formulate best fit vectors
 	if Obj_5:
-		Obj_5.rotation_degrees = Vector3(randf_range(0.0, 360.0), 0.0, randf_range(0.0, 360.0))
+		Obj_5.rotation_degrees = order.pop_back()
 
 func _ready() -> void:
 	placement()
