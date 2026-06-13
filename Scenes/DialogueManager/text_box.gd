5de89ab9d3c6ada3428ba5fba9f668abbbadfcc3
ROCINANTE\mbrissie <mbrissie@gmail.com>
2026-06-13 19:58:41 -0400

    minor cleanup, in the middle of dialogue windows functional

--- Scenes/DialogueManager/text_box.gd
+++ Scenes/DialogueManager/text_box.gd
@@ -1,10 +1,18 @@
 extends MarginContainer
 
-@onready var margin_label : MarginContainer = $MarginContainer
-@onready var label : Label = $MarginContainer/Label
+@onready var margin_label : MarginContainer = $ControlMargin/MarginContainer
+@onready var label : Label = $ControlMargin/MarginContainer/Label
 @onready var timer : Timer = $LetterDisplayTimer
 @onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer
-@onready var next_indicator : AnimatedSprite2D = $NinePatchRect/Control/NextIndicator
+@onready var next_indicator : AnimatedSprite2D = $ControlIndicator/NextIndicator
+@onready var next_f : Label = $ControlIndicator/NextIndicator/Label
+@onready var DialogueBox : Sprite2D = $NewDialogueBoxChromakey
+
+@onready var BaubleLeft : Sprite2D = $DialogueBaubleLeft
+@onready var BaubleMiddle : Sprite2D = $DialogueBaubleMiddle
+@onready var BaubleRight : Sprite2D = $DialogueBaubleRight
+@onready var upper_flourish : Sprite2D = $DialogueBoxUpperFlourish
+
 
 const  MAX_WIDTH : float = 640.0
 const MAX_HEIGHT : float = 170.0
@@ -20,22 +28,22 @@ signal finished_displaying()
 
 func tween_text_box():
 	if not DialogueManager.already_tweened:
+		DialogueBox.scale = Vector2(0.0, 0.0)
 		DialogueManager.already_tweened = true
 		var tween_x = get_tree().create_tween()
 		var tween_y = get_tree().create_tween()
-		var tween_pos_x = get_tree().create_tween()
 		
-		var tween_x_tweener = tween_x.tween_property(self, "size:x",  MAX_WIDTH, .2)
+		var tween_x_tweener = tween_x.tween_property(DialogueBox, "scale:x",  1.0, .3)
 		tween_x_tweener.set_trans(Tween.TRANS_SINE)
 		tween_x_tweener.set_ease(Tween.EASE_OUT)
 		tween_x.play()
 		
-		var tween_pos_x_tweener = tween_pos_x.tween_property(self, "position:x", 320.0, .2)
-		tween_pos_x_tweener.set_trans(Tween.TRANS_SINE)
-		tween_pos_x_tweener.set_ease(Tween.EASE_OUT)
-		tween_pos_x.play()
+		#var tween_pos_x_tweener = tween_pos_x.tween_property(self, "position:x", 320.0, .2)
+		#tween_pos_x_tweener.set_trans(Tween.TRANS_SINE)
+		#tween_pos_x_tweener.set_ease(Tween.EASE_OUT)
+		#tween_pos_x.play()
 		
-		var tween_y_tweener = tween_y.tween_property(self, "size:y",  MAX_HEIGHT, .2).set_delay(.2)
+		var tween_y_tweener = tween_y.tween_property(DialogueBox, "scale:y",  1.0, .3).set_delay(.1)
 		tween_y_tweener.set_trans(Tween.TRANS_SINE)
 		tween_y_tweener.set_ease(Tween.EASE_OUT)
 		tween_y.play()
@@ -44,12 +52,10 @@ func tween_text_box():
 			tween_x.kill()
 		if tween_y and tween_y.is_valid():
 			tween_y.kill()
-		if tween_pos_x and tween_pos_x.is_valid():
-			tween_pos_x.kill()
+		#if tween_pos_x and tween_pos_x.is_valid():
+			#tween_pos_x.kill()
 	else:
-		size.x = MAX_WIDTH
-		size.y = MAX_HEIGHT
-		position.x = 320.0
+		DialogueBox.scale = Vector2(1.0, 1.0)
 
 func display_text(text_to_display: String, speech_sfx: AudioStream):
 	text = text_to_display
@@ -68,11 +74,11 @@ func _display_letter():
 	if text.length() < 50:
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 		label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-		margin_label.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+		#margin_label.size_flags_vertical = Control.SIZE_SHRINK_CENTER
 	else:
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
 		label.vertical_alignment = VERTICAL_ALIGNMENT_TOP
-		margin_label.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
+		#margin_label.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
 	
 	#this is to skip dialogue windows
 	if Input.is_action_pressed("advance_dialogue") and letter_index > 2:
@@ -87,6 +93,9 @@ func _display_letter():
 	if letter_index >= text.length():
 		finished_displaying.emit()
 		next_indicator.visible = true
+		next_indicator.play()
+		await next_indicator.animation_finished
+		next_f.visible = true
 		AudioManager.sfx_play(AudioManager.sfx_blip, 1.0)
 		return
 		
