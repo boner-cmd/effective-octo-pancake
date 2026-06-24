ee88c01cc6e0b01deb8697fe9100ba11b6c496c0
bcmd <54084132+boner-cmd@users.noreply.github.com>
2026-06-24 02:27:58 -0400

    removed setter, don't think it was doing anything

--- Scenes/honk_counter.gd
+++ Scenes/honk_counter.gd
@@ -1,10 +1,7 @@
 extends Node
 ## TODO document honk counter
 
-## getter should only be used when preparing to store the total for save data. A getter is used here
-## to avoid doing math or otherwise directly interacting with the honk number.
-var honk_total : int = 0 : 
-	get : return honk_total
+var honk_total : int = 0
 
 func add_honk() -> void: honk_total += 1
 
