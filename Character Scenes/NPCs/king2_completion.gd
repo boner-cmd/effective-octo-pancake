8549b1ea763cd698b1152329ebc94adba8391fb5
ROCINANTE\mbrissie <mbrissie@gmail.com>
2026-07-02 17:27:03 -0400

    more stars on astro, fix transparency solving for gibberish/michaelwave

--- Character Scenes/NPCs/king2_completion.gd
+++ Character Scenes/NPCs/king2_completion.gd
@@ -1,7 +1,7 @@
 extends Node3D
 @onready var door_credits: Node3D = $"../../../Door_Credits"
 @onready var credits_door_locator: Node3D = $Credits_Door_Locator
-@export var debug_victory = true
+@export var debug_victory = false
 
 func on_completion():
 	if QuestManager.has_completed(QuestManager.CharacterName.KING_2):
